e188fe826130a7a10aa60b6bccafcf02ec227467
Alya <alena.kovinko@rambler.ru>
2016-02-15 19:23:02 +0300

    New and fixed files

--- Python/grep.py
+++ Python/grep.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import argparse
 import re
 import glob
@@ -8,6 +9,7 @@ parser.add_argument('pattern',type=str,help='Шаблон поиска')
 parser.add_argument('-i',action='store_true',help='Игнорировать регистр')
 parser.add_argument('-m',type=int,help='Остановиться после указанного числа совпадений')
 parser.add_argument('-n',action='store_true',help='Печатать номер строки вместе с выходными строками')
+parser.add_argument('-e',action='store_true',help='Шаблон - регулярное выражение')
 parser.add_argument('files',metavar='file',type=str,nargs='*',help='файлы для обработки')
 
 args = parser.parse_args()
@@ -15,7 +17,16 @@ args = parser.parse_args()
 def printuy(text,file='stdin'):
     how_match=0
     for i, line in enumerate(text):
-        if (args.i and re.search(args.pattern, line, re.IGNORECASE)) or (re.search(args.pattern, line)):
+        flag=False
+        if (args.i and args.e):
+            flag=re.search(args.pattern, line, re.IGNORECASE)
+        elif args.e:
+            flag=re.search(args.pattern,line)
+        elif args.i:
+            flag=args.pattern.lower() in line.lower()
+        else:
+            flag=args.pattern in line
+        if flag:#(args.i and re.search(args.pattern, line, re.IGNORECASE)) or (re.search(args.pattern, line)):
             how_match+=1
             if args.m:
                 if how_match > args.m:
@@ -23,7 +34,7 @@ def printuy(text,file='stdin'):
             if args.n:
                 print(file,i,':',line)
             else:
-                print(file,line)    
+                print(file,line)
 
 if args.files:
     for pattern in args.files:
